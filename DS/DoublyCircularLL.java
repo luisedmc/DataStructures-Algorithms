23b9408f5158af4caac723570dc65f48a013a379
Luis Eduardo M. <luiseduardomc019@gmail.com>
2024-05-20 15:24:09 -0300

    doubly circular ll

---
+++ DS/DoublyCircularLL.java
@@ -0,0 +1,81 @@
+public class DoublyCircularLL {
+  private Node head, tail;
+  private int size;
+
+  DoublyCircularLL() {
+    head = tail = null;
+    size = 0;
+  }
+
+  public void Insert(int data) {
+    Node newNode = new Node(data);
+
+    if (isEmpty()) {
+      head = tail = newNode;
+      head.setNext(head);
+      head.setPrev(tail);
+    } else {
+      tail.setNext(newNode);
+      newNode.setPrev(tail);
+      newNode.setNext(head);
+      head.setPrev(newNode);
+      tail = newNode;
+    }
+
+    size++;
+  }
+
+  public Node Remove() {
+    if (isEmpty()) return null;
+
+    Node temp = head;
+
+    if (head == tail) {
+      head = tail = null;
+    } else {
+      head = head.getNext();
+      head.setPrev(tail);
+      tail.setNext(head);
+    }
+
+    size--;
+    return temp;
+  }
+
+  public void Change(int data, int newData) {
+    Node temp = head;
+
+    for (int i = 0; i < size; i++) {
+      if (temp.getData() == data) {
+        temp.setData(newData);
+        return;
+      }
+      temp = temp.getNext();
+    }
+  }
+
+  public boolean Search(int data) {
+    Node temp = head;
+
+    for (int i = 0; i < size; i++) {
+      if (temp.getData() == data) return true;
+      temp = temp.getNext();
+    }
+
+    return false;
+  }
+
+  private boolean isEmpty() {
+    return head == null && tail == null;
+  }
+
+  public void Print() {
+    Node temp = head;
+
+    for (int i = 0; i < size; i++) {
+      System.out.print(temp.getData() + " ");
+      temp = temp.getNext();
+    }
+    System.out.println();
+  }
+}
